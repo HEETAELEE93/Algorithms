b5c70153ce9afdb78b412454653de8da3aaaec09
heetaelee93 <heetaelee93@gmail.com>
2021-09-19 17:37:56 +0900

    no01 findChar

--- Inflearn_JavaAlgorithm/src/no1_string/No01_FindChar.java
+++ Inflearn_JavaAlgorithm/src/no1_string/No01_FindChar.java
@@ -2,14 +2,31 @@ package no1_string;
 
 import java.util.Scanner;
 
-
+class FindChar {
+	public int solution(String str, char t) {
+		int answer = 0;
+		
+		str = str.toUpperCase();
+		t = Character.toUpperCase(t);
+		
+		for(char c : str.toCharArray()) {
+			if(c == t) {
+				answer++;
+			}
+		}
+		return answer;
+	}
+}
 
 public class No01_FindChar {
 	public static void main(String[] args) {
-		Scanner in=new Scanner(System.in);
-	    int input1 = in.nextInt();
-	    int input2 = in.nextInt();
-	    System.out.println(input1 + input2);
+		Scanner in = new Scanner(System.in);
+	    String str = in.next();
+	    char c = in.next().charAt(0);
+	    
+	    FindChar sol = new FindChar();
+	    
+	    System.out.println(sol.solution(str, c));
 	    return ;
 	}
 }
