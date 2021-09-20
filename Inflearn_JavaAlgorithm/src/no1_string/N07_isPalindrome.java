23e86908fa1e42a4513fae5c6d821435f70d9260
heetaelee93 <heetaelee93@gmail.com>
2021-09-20 18:15:18 +0900

    is Palindrome

--- Inflearn_JavaAlgorithm/src/no1_string/N07_isPalindrome.java
+++ Inflearn_JavaAlgorithm/src/no1_string/N07_isPalindrome.java
@@ -1,9 +1,30 @@
 package no1_string;
 
+import java.util.Scanner;
+
 class isPalindrom {
 	public String solution (String str) {
-		String answer = "";
+		String answer = "YES";
+		str = str.toLowerCase();
+		int len = str.length();
 		
+		for(int i = 0; i < len/2; i++) {
+			if(str.charAt(i) != str.charAt( len - i - 1)) {
+				return "NO";
+			}
+		}
+		
+		return answer;
+	}
+	
+	public String solution2 (String str) {
+		String answer = "YES";
+		
+		String tmp = new StringBuilder(str).reverse().toString();
+		
+		if(str.equals(tmp)) {
+			return "NO";
+		}
 		
 		return answer;
 	}
@@ -11,6 +32,11 @@ class isPalindrom {
 
 public class N07_isPalindrome {
 	public static void main(String[] args) {
-
+//		Scanner sc = new Scanner(System.in);
+//		String str = sc.next();
+		String str = "gooG";
+		isPalindrom sol = new isPalindrom();
+		System.out.println(sol.solution(str));
+		System.out.println(sol.solution2(str));
 	}
 }
