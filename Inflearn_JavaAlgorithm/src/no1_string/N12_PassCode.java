60308fed3047f6f8b4a0c965dcd08f6c6fbbf114
heetaelee93 <heetaelee93@gmail.com>
2021-09-23 23:11:44 +0900

    PassCode

---
+++ Inflearn_JavaAlgorithm/src/no1_string/N12_PassCode.java
@@ -0,0 +1,22 @@
+package no1_string;
+
+import java.util.Scanner;
+
+class PassCode {
+	public String solution(int n, String s) {
+		String answer = "";
+		
+		return answer;
+	}
+}
+
+public class N12_PassCode {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		String str = sc.next();
+		
+		PassCode sol = new PassCode();
+		System.out.println(sol.solution(n, str));
+	}
+}
