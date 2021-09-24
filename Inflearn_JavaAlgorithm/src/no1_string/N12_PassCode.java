592a20d811dc397fbcd44bb396ebdd90ebf23976
heetaelee93 <heetaelee93@gmail.com>
2021-09-24 17:25:32 +0900

    Pass Code

--- Inflearn_JavaAlgorithm/src/no1_string/N12_PassCode.java
+++ Inflearn_JavaAlgorithm/src/no1_string/N12_PassCode.java
@@ -6,17 +6,47 @@ class PassCode {
 	public String solution(int n, String s) {
 		String answer = "";
 		
+		for(int i = 0; i < s.length(); i+=7) {
+			String binaryNum = "";
+			for(int j = i; j < i + 7; j++) {
+				if(s.charAt(j) == '#') {
+					binaryNum += "1";
+				} else {
+					binaryNum += "0";
+				}
+			}
+			int decimal = Integer.parseInt(binaryNum, 2);
+			answer += (char) decimal;
+		}
+		return answer;
+	}
+	
+	public String solution2(int n, String s) {
+		String answer = "";
+		
+		for(int i = 0; i < n; i++) {
+			String tmp = s.substring(0, 7).replace('#', '1').replace('*', '0');
+			
+			int num = Integer.parseInt(tmp, 2);
+			answer += (char) num;
+			s = s.substring(7);
+		}
+		
 		return answer;
 	}
 }
 
 public class N12_PassCode {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		String str = sc.next();
+//		Scanner sc = new Scanner(System.in);
+//		int n = sc.nextInt();
+//		String str = sc.next();
+		
+		int n = 4;
+		String str = "#****###**#####**#####**##**";
 		
 		PassCode sol = new PassCode();
 		System.out.println(sol.solution(n, str));
+		System.out.println(sol.solution2(n, str));
 	}
 }
---
+++ Inflearn_JavaAlgorithm/src/no2_array/N01_PrintBigNum.java
@@ -0,0 +1,11 @@
+package no2_array;
+
+class PrintBigNum {
+	
+}
+
+public class N01_PrintBigNum {
+	public static void main(String[] args) {
+
+	}
+}
