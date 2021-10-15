6f13a1c83f78939c8f3f870a02339802e0e36b91
heetaelee93 <heetaelee93@gmail.com>
2021-10-15 23:52:40 +0900

    CorrectParenthesis

--- Inflearn_JavaAlgorithm/src/no5_StackQueue/N01_CorrectParenthesis.java
+++ Inflearn_JavaAlgorithm/src/no5_StackQueue/N01_CorrectParenthesis.java
@@ -27,7 +27,17 @@ class CorrectParenthesis {
 	public String solution2(String str) {
 		String answer = "YES";
 		Stack<Character> stack = new Stack<Character>();
-		
+		for(char x : str.toCharArray()) {
+			if(x == '(') stack.push(x);
+			else {
+				if(stack.isEmpty()) 
+					return "NO";
+				stack.pop();
+			}
+		}
+		if(!stack.isEmpty()) {
+			return "NO";
+		}
 		return answer;
 	}
 }
