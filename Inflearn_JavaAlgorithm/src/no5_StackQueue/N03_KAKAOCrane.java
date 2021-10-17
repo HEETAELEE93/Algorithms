63c08adcff93049fc8b15a334734e0e9bb1f9724
heetaelee93 <heetaelee93@gmail.com>
2021-10-17 19:54:02 +0900

    Postfix

--- Inflearn_JavaAlgorithm/src/no5_StackQueue/N03_KAKAOCrane.java
+++ Inflearn_JavaAlgorithm/src/no5_StackQueue/N03_KAKAOCrane.java
@@ -4,27 +4,50 @@ import java.util.Scanner;
 import java.util.Stack;
 
 class KAKAOCrane {
-    public int solution(int[][] board, int[] moves) {
-        int answer = 0;
-                Stack<Integer> st = new Stack<Integer>();
-
-        for(int i = 0; i < moves.length; i++) {
-            for(int j = 0; j < board.length; j++) {
-                if(board[j][moves[i]-1] != 0) {
-                    if(!st.isEmpty() && st.peek() == board[j][moves[i]-1]) {
-                        st.pop();
-                        answer++;
-                    } else {
-                        st.push(board[j][moves[i]-1]);
-                    }
-                    board[j][moves[i]-1] = 0;
-                    break;
-                }
-            }
-        }
-
-        return answer*2;
-    }
+	public int solution(int[][] board, int[] moves) {
+		int answer = 0;
+		Stack<Integer> st = new Stack<Integer>();
+		
+		for(int m : moves) {
+			m = m-1;
+			for(int i = 0; i < board.length; i++) {
+				if(board[i][m] != 0) {
+					if(!st.isEmpty() && st.peek() == board[i][m]) {
+						st.pop();
+						answer++;
+					} else {
+						st.push(board[i][m]);
+					}
+					board[i][m] = 0;
+					break;
+				}
+			}
+		}
+		return answer*2;
+	}
+	
+	public int solution2(int[][] board, int[] moves) {
+		int answer = 0;
+		Stack<Integer> st = new Stack<Integer>();
+		
+		for(int pos : moves) {
+			for(int i = 0; i < board.length; i++) {
+				if(board[i][pos-1] != 0) {
+					int tmp = board[i][pos-1];
+					board[i][pos-1] = 0;
+					if(!st.isEmpty() && tmp == st.peek()) {
+						answer+=2;
+						st.pop();
+					} else {
+						st.push(tmp);
+					}
+					break;
+				}
+			}
+		}
+		
+		return answer;
+	}
 }
 public class N03_KAKAOCrane {
 	public static void main(String[] args) {
@@ -53,6 +76,7 @@ public class N03_KAKAOCrane {
 		int m = 8;
 		int[] moves = {1,5,3,5,1,2,1,4};
 		KAKAOCrane sol = new KAKAOCrane();
-		System.out.println(sol.solution(board, moves));
+//		System.out.println(sol.solution(board, moves));
+		System.out.println(sol.solution2(board, moves));
 	}
 }
---
+++ Inflearn_JavaAlgorithm/src/no5_StackQueue/N04_Postfix.java
@@ -0,0 +1,43 @@
+package no5_StackQueue;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+class Postfix {
+	public int solution(String str) {
+		int answer = 0;
+		Stack<Integer> stack = new Stack<Integer>();
+		
+		for(char x : str.toCharArray()) {
+			if(Character.isDigit(x)) {
+				stack.push(x - 48);
+			} else {
+				int rt = stack.pop();
+				int lt = stack.pop();
+				if(x == '+') {
+					stack.push(lt + rt);
+				} else if(x =='-') {
+					stack.push(lt - rt);
+				} else if(x =='*') {
+					stack.push(lt * rt);
+				} else if(x =='/') {
+					stack.push(lt / rt);
+				}
+			}
+		}
+		answer = stack.pop();
+		return answer;
+	}
+}
+
+public class N04_Postfix {
+	public static void main(String[] args) {
+//		Scanner sc = new Scanner(System.in);
+//		String str = sc.next();
+		
+		String str = "352+*9-";
+		
+		Postfix sol = new Postfix();
+		System.out.println(sol.solution(str));
+	}
+}
