36854d6695b7e89008dacd206c57c19c1c220038
heetaelee93 <heetaelee93@gmail.com>
2021-11-04 22:52:44 +0900

    algorithm

---
+++ Inflearn_JavaAlgorithm/src/no7_recursiveTreeGraph/N13_ShortestGraph_bfs.java
@@ -0,0 +1,25 @@
+package no7_recursiveTreeGraph;
+
+class ShortestGraph {
+	
+}
+public class N13_ShortestGraph_bfs {
+	public static void main(String[] args) {
+		ShortestGraph sol = new ShortestGraph();
+		int n = 5;	// 정점의 수
+		int m = 9;	// 간선의 수
+		
+		int[][] dirs = {
+				{5,9},
+				{1,2},
+				{1,3},
+				{1,4},
+				{2,1},
+				{2,3},
+				{2,5},
+				{3,4},
+				{4,2},
+				{4,5},
+		};
+	}
+}
