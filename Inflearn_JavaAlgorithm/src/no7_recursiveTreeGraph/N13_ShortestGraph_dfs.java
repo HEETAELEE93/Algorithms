9f83f36a0f329ec18ff02c33c4fea4445e6ccd5e
heetaelee93 <heetaelee93@gmail.com>
2021-11-12 16:18:00 +0900

    ShortestGraph_dfs

--- Inflearn_JavaAlgorithm/src/no7_recursiveTreeGraph/N13_ShortestGraph_dfs.java
+++ Inflearn_JavaAlgorithm/src/no7_recursiveTreeGraph/N13_ShortestGraph_dfs.java
@@ -3,60 +3,58 @@ package no7_recursiveTreeGraph;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Stack;
 
 class ShortestGraphDFS {
 	int[] ch;
 	int n, m, answer = 0;
 	ArrayList<ArrayList<Integer>> graph;
-	ArrayList<ArrayList<Integer>> list;
-	int cnt = 1;
+	Stack<Integer> stack = new Stack<Integer>();;
+	ArrayList<ArrayList<Integer>> list = new ArrayList<ArrayList<Integer>>();
+
 	public void solution_dfs(int n, int m, int[][] dirs) {
 		this.n = n;
 		this.m = m;
 		graph = new ArrayList<ArrayList<Integer>>();
-		list = new ArrayList<ArrayList<Integer>>();
-		
-		for(int i = 0; i <= n; i++) {
+
+		for (int i = 0; i <= n; i++) {
 			graph.add(new ArrayList<Integer>());
 		}
-		
-		for(int i = 1; i <= m; i++) {
+
+		for (int i = 1; i <= m; i++) {
 			int a = dirs[i][0];
 			int b = dirs[i][1];
 			graph.get(a).add(b);
 		}
-		
+
 		ch = new int[n + 1];
 		ch[1] = 1;
+		stack.push(1);
 		dfs(1);
 		System.out.println(answer);
-		for(ArrayList<Integer> li : list) {
+		for (ArrayList<Integer> li : list) {
 			System.out.println(li);
 		}
 	}
-	
+
 	public void dfs(int v) {
-		if(v == n) {
-			list.add(new ArrayList<Integer>());
-			for(int i = 0; i < ch.length; i++) {
-				if(ch[i] == 1) {
-					list.get(answer).add(i);					
-				}
-			}
+		if (v == n) {
 			answer++;
-			
-		}
-		for(int nv : graph.get(v)) {
-			if(ch[nv] == 0) {
-				ch[nv] = cnt++;
-				dfs(nv);
-				ch[nv] = 0;
-				cnt--;
+			list.add(new ArrayList<Integer>(stack));
+		} else {
+			for (int nv : graph.get(v)) {
+				if (ch[nv] == 0) {
+					ch[nv] = 1;
+					stack.push(nv);
+					dfs(nv);
+					stack.pop();
+					ch[nv] = 0;
+				}
 			}
 		}
 	}
-	
 }
+
 public class N13_ShortestGraph_dfs {
 	public static void main(String[] args) {
 		ShortestGraphDFS sol = new ShortestGraphDFS();
@@ -72,8 +70,8 @@ public class N13_ShortestGraph_dfs {
 				{4,2},
 				{4,5}
 		};
-		int n = dirs[0][0];	// 정점의 수
-		int m = dirs[0][1];	// 간선의 수
+		int n = dirs[0][0]; // 정점의 수
+		int m = dirs[0][1]; // 간선의 수
 		sol.solution_dfs(n, m, dirs);
 	}
 }
